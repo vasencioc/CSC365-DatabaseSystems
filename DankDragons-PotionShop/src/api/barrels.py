a1fda299df68b6cefc1d5404800c37002e2b87f5
vasencioc <124462752+vasencioc@users.noreply.github.com>
2024-04-14 18:53:25 -0700

    barrel 100 to 1

--- DankDragons-PotionShop/src/api/barrels.py
+++ DankDragons-PotionShop/src/api/barrels.py
@@ -53,7 +53,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     #traverse catalog
     for barrel in wholesale_catalog:
         # if GREEN barrel in catalog
-        if barrel.potion_type == [0,100, 0, 0]:
+        if barrel.potion_type == [0,1, 0, 0]:
             if num_green < 10 and barrel.quantity and barrel.price < wallet: 
                 wallet -= barrel.price
                 purchase.append({
